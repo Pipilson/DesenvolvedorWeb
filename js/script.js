d073301b7136b9175a816e62f53d320810900177
Pipilson <pietro.varraschim@hotmail.com>
2022-10-31 20:49:11 -0300

    Introdução à JavaScript
    Adicionado arquivo .js que faz somas numéricas

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//alert("Olá mundo"); // mensagem de alerta
+//var resposta = confirm("Deseja excluir?"); // variável nome = método()
+//console.log(resposta); // para visualizar resposta
+var a = 10;
+var b = 33;
+
+function somar(a, b){
+	return a + b;
+}
+
+console.log(somar(2, 5));
