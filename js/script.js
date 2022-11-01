b698e46140cf7d6dfb51088593d36a16e88683e3
Pipilson <pietro.varraschim@hotmail.com>
2022-10-31 21:18:38 -0300

    Interação do usuário com o site
    Adicionado no site uma função JavaScript que soma dois números inseridos no site por um usuário

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 //alert("Olá mundo"); // mensagem de alerta
 //var resposta = confirm("Deseja excluir?"); // variável nome = método()
 //console.log(resposta); // para visualizar resposta
-var a = 10;
-var b = 33;
+const a = 10; // 3 formas de declarar variável: var, const (não muda), let (para escopo)
+const b = 33;
 
 function somar(a, b){
-	return a + b;
+	return parseInt(a) + parseInt(b);
 }
 
-console.log(somar(2, 5));
+//console.log(somar(2, 5));
+
+document.querySelector("#calcular").addEventListener("click", function(){
+	let valorA = document.querySelector("#valorA").value // #id
+	let valorB = document.querySelector("#valorB").value
+
+	if (valorA.length > 0 && valorB.length > 0) {
+		//alert(parseInt(valorA) + parseInt(valorB));
+		alert(somar(valorA, valorB));
+	}
+	else{
+		alert("Digite os valores!");
+	}
+
+});
